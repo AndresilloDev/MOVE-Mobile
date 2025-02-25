84b28897cd16e4878143af368f72f2888aac52b4
AndresilloDev <20233tn107@utez.edu.mx>
2025-02-25 00:40:22 -0600

    refactor: Refactorice el Login, utilizando el componente Header

--- Frontend/screens/LoginScreen.jsx
+++ Frontend/screens/LoginScreen.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { AuthContext } from "../context/AuthContext";
+import Header from "../components/Header";
 
 const LoginScreen = ( {navigation} ) => {
   const { login } = useContext(AuthContext);
@@ -8,36 +9,49 @@ const LoginScreen = ( {navigation} ) => {
   const [password, setPassword] = useState("");
 
   return (
-    <View className="flex-1 justify-center items-center bg-gray-100">
-          <Image
+    <View className="flex-1">
+      	<Image
             source={require("../assets/bg.png")} 
             className="absolute top-0 left-0 w-fill h-full" 
             style={{ width: "600%", height: "100%"}}
             resizeMode="cover" 
-          />
-      <Text className="text-2xl font-bold mb-5">Inicia sesión en MOVE</Text>
-      <TextInput
-        className="w-4/5 h-10 border border-gray-300 rounded-lg mb-2 p-2 bg-white"
-        placeholder="Correo electrónico"
-        autoCapitalize="none"
-        onChangeText={(text) => setUsername(text.trim().toLowerCase())}
-      />
-      <TextInput
-        className="w-4/5 h-10 border border-gray-300 rounded-lg mb-4 p-2 bg-white"
-        placeholder="Contraseña"
-        secureTextEntry
-        onChangeText={setPassword}
-      />
-      <Text className="w-4/5 text-right text-sm mb-4" onPress={() => navigation.navigate("ForgotPassword")}>
-        Olvidaste tu contraseña?
-      </Text>
-      <TouchableOpacity
-        className="bg-primary p-2 rounded-lg mt-3 w-4/5 items-center"
-        style={{ borderColor: "#BBD04E" }}
-        onPress={() => login(username, password)}
-      >
-        <Text className="text-lg">Iniciar Sesión</Text>
-      </TouchableOpacity>
+      	/>
+
+        <Header navigation={navigation} />
+
+		<View className="mt-40 items-center justify-center">
+
+			<Text className="text-3xl font-bold mb-16">Inicia sesión en MOVE</Text>
+			
+			<TextInput
+				className="w-4/5 h-10 border border-gray-500 rounded-lg mb-8 p-2 bg-white"
+				placeholder="Correo electrónico"
+				placeholderTextColor="#606266"
+				autoCapitalize="none"
+				onChangeText={(text) => setUsername(text.trim().toLowerCase())}
+			/>
+
+			<TextInput
+				className="w-4/5 h-10 border border-gray-500 rounded-lg mb-2 p-2 bg-white"
+				placeholder="Contraseña"
+				placeholderTextColor="#606266"
+				secureTextEntry
+				onChangeText={setPassword}
+			/>
+
+			<Text className="w-4/5 text-right text-sm mb-8 underline" onPress={() => navigation.navigate("ForgotPassword")}>
+				Olvidaste tu contraseña?
+			</Text>
+			
+			<TouchableOpacity
+				className="bg-action-primary rounded-lg px-8 border-lines"
+				style={{ borderWidth: 1.3, paddingVertical: 5, width: "80%" }}
+				onPress={() => login(username, password)}
+			>
+				<Text className="text-primary text-lg text-center">Iniciar Sesión</Text>
+
+			</TouchableOpacity>
+		</View>
     </View>
   );
 };
